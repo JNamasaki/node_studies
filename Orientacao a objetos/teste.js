22402cd348beef670c47cde3977ca9139444cd82
JNamasaki(Jessica Rodrigues) <r.jmrsilva.j@gmail.com>
2023-01-27 21:41:29 -0300

    teste

---
+++ Orientacao a objetos/teste.js
@@ -0,0 +1,17 @@
+// shouLd be meaningfuL
+// cannot start with a number (Iname)
+// cannot contain a space or hyphen (-)
+// case sentitve
+
+let firstName = 'ascarrtt' ;
+let lastName = 'duollar';
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate) ;
+let name ='ascarrtt'; // String LiteriaL
+
+let age = 18; // Number Literial
+let isApproved = false; // Boolean LiteriaL
+firstName = undefined;
+
+let selectedcolor = null;
